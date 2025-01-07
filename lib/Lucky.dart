e8a4014471c13eef671faa8cedad2476b3f7a04c
Ekam Bhatia <ekambhatia@Ekams-MacBook-Pro.local>
2025-01-07 17:59:36 +0530

    dart 2

---
+++ lib/Lucky.dart
@@ -0,0 +1,13 @@
+import "dart:io";
+import "dart:math";
+
+
+class Lucky {
+  Lucky();
+  int min = int.parse(Platform.environment["MIN"] ?? "0");
+  int max = int.parse(Platform.environment["MAX"] ?? "10");
+
+  int randomNumber() {
+    return min + Random().nextInt(max - min);
+  }
+}
---
+++ test/dart_application_1_test.dart
@@ -0,0 +1,6 @@
+import 'package:dart_application_1/Lucky.dart';
+import 'package:test/test.dart';
+
+void main() {
+  
+}
